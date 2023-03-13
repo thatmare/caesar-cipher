771f72d06aedeae7e2adc4064e9d3c275a4e9c33
Marissa Vargas <marissa.lorena@hotmail.com>
2023-03-13 17:44:59 -0600

    resolví el algoritmo del cifrado y convertir el input number en valor numérico, pero debo encontrar la manera de agregar los parámetros del input message y del offset a la función

--- src/index.js
+++ src/index.js
@@ -3,19 +3,43 @@ import cipher from './cipher.js';
 console.log(cipher);
 
 const inputMessage = document.getElementById('inputMessage');
+let inputValue = inputMessage.value;
 const offset = document.getElementById('offset');
+let offsetValue = offset.value; 
 const codeBtn = document.getElementById('codeBtn');
-const result = document.getElementById('result');
 
-offset.addEventListener('input', function (){
-    let offsetValue = offset.valueAsNumber;
-    console.log(offsetValue);
-});
+function toNumber(param) {
+    const offsetParam = Number(param);
+    
+    caesarCiphher();
+}
+
+function caesarCiphher(inputValue, ) {
+    let result = "";
+    for (let i=0; i<string.length; i++) {
+        let letterASCII = string[i].charCodeAt();
+        if (letterASCII >= 65 && letterASCII <= 90){
+            result += String.fromCharCode(((letterASCII - 65 + offset)%26)+65);
+        }
+    }
+
+    return result;
+}
+
+//OFFICE HOUR LUNES 13
+//codeBtn.addEventListener('click', function (){
+  //  prueba(offset.value);
+//});
 
 //codeBtn.addEventListener('click', function(){
   //  console.log(inputMessage.value)
 //})
 
+//OFFICE HOUR LUNES 13
+//function prueba(param){
+  //  const offsetParam = Number(param);
+    //console.log(offsetParam)
+//}
 
 //let i=0; i < string.length; i++ es una construcción para iterar sobre todos los caracteres de una cadena de texto. Estamos declarando la variable "i", que debe ser igual a 0 pues ese es el valor o posición inicial. "i < string.length" nos indica que va a iterar siempre y cuando la variable i sea menor que la longitud de string, es decir, se va a detener cuando haya iterado sobre todos los caracteres de string. "i++" es la expresión de incremento, pues en cada iteración el valor de i aumentará uno para así poder avanzar. 
 //for (let i=0; i < string.length; i++) {
