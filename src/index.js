ac7c725b8cc76db649d0d525f9c54fa857f5b6c5
Marissa Vargas <marissa.lorena@hotmail.com>
2023-03-14 16:35:12 -0600

    cambios martes 14

--- src/index.js
+++ src/index.js
@@ -8,62 +8,37 @@ const codeBtn = document.getElementById('codeBtn');
 const decodeBtn = document.getElementById('decodeBtn');
 
 function caesarCipher() {
-  let inputValue = inputMessage.value;
-  let offsetValue = Number(offset.value); 
-  let result = "";
-    for (let i=0; i<inputValue.length; i++) {
-        let letterASCII = inputValue[i].charCodeAt();
-        if (letterASCII >= 65 && letterASCII <= 90){
-            result += String.fromCharCode(((letterASCII - 65 + offsetValue)%26)+65);
-        } else if (letterASCII >= 97 && letterASCII <= 122) {
-            result += String.fromCharCode(((letterASCII - 97 + offsetValue)%26)+97);
+  let inputValue = inputMessage.value; //recuperamos el valor ingresada por la usuaria en el área de texto
+  let offsetValue = Number(offset.value); //recuperamos el valor de desplazamiento en el input number, lo convertimos a valor numérico porque es string
+  let result = ""; //es un valor vacío porque aquí almacenaremos el resultado
+    for (let i=0; i<inputValue.length; i++) { //es un bucle nos sirve para iterar o recorrer los valores que recibiremos (los caracteres de la cadena de texto) siempre y cuando se cumplan las condiciones siguientes. Declaramos la variable i, que inicialmente está en la posición 0 (i=0); la variable i no deberá sobrepasar la longitud de la cadena de texto (i<inputValue.length); y la variable i, que inicialmente es 0, va a ir avanzando una posición cada bucle
+        let letterASCII = inputValue[i].charCodeAt(); //declaro la variable letterASCII para almacenar el código ASCII de cada caracter, con el método charCodeAt()
+        if (letterASCII >= 65 && letterASCII <= 90){ //empiezan las condiciones: si la variable ASCII es igual o menor a 65 (A en ASCII) e igual o mayor a 90 (Z en ASCII), se ejecutará lo siguiente...
+            result += String.fromCharCode(((letterASCII - 65 + offsetValue)%26)+65); //se le va a agregar el valor a la variable de result que va a ser igual a el caracter recuperado de la operación (((x-65+n)%26)+65)
+        } else if (letterASCII >= 97 && letterASCII <= 122) { 
+            result += String.fromCharCode(((letterASCII - 97 + offsetValue)%26)+97); //repetimos lo anterior, pero para minúsculas (a es 97 en ASCII y z es 122 en ASCII)
         }
     }
 
     window.alert(result);
 }
 
-codeBtn.addEventListener('click', caesarCipher)
+codeBtn.addEventListener('click', caesarCipher) //Aquí se ejecuta la función, al hacer clic en el botón.
 
-//function caesarDecipher() {
-//  let inputValue = inputMessage.value;
-//  let offsetValue = Number(offset.value);
-//  let result = "";
-    //for (let i=0; i<inputValue.length; i++) {
-      //let letterASCII = inputValue[i].charCodeAt();
-      //if (letterASCII >= 65 && letterASCII <= 90) {
-        //result += String.fromCharCode(((letterASCII - 65 - offsetValue + 26)%26)+65);
-      //} else if (letterASCII >= 97 && letterASCII <= 122) {
-        //result += String.fromCharCode(((letterASCII - 97 - offsetValue)%26)+97);
-      //}
-    //}
+function caesarDecipher() {
+  let inputValue = inputMessage.value;
+  let offsetValue = Number(offset.value);
+  let result = "";
+    for (let i=0; i<inputValue.length; i++) {
+      let letterASCII = inputValue[i].charCodeAt();
+      if (letterASCII >= 65 && letterASCII <= 90) {
+        result += String.fromCharCode(((letterASCII - 65 - offsetValue + 26)%26)+65);
+      } else if (letterASCII >= 97 && letterASCII <= 122) {
+      result += String.fromCharCode(((letterASCII - 97 - offsetValue)%26)+97);
+      }
+    }
 
-    //window.alert(result);
-//}
+    window.alert(result);
+}
 
-decodeBtn.addEventListener('click', function(){
-  alert("hola");
-});
-
-//OFFICE HOUR LUNES 13
-//codeBtn.addEventListener('click', function (){
-  //  prueba(offset.value);
-//});
-
-//codeBtn.addEventListener('click', function(){
-  //  console.log(inputMessage.value)
-//})
-
-//OFFICE HOUR LUNES 13
-//function prueba(param){
-  //  const offsetParam = Number(param);
-    //console.log(offsetParam)
-//}
-
-//let i=0; i < string.length; i++ es una construcción para iterar sobre todos los caracteres de una cadena de texto. Estamos declarando la variable "i", que debe ser igual a 0 pues ese es el valor o posición inicial. "i < string.length" nos indica que va a iterar siempre y cuando la variable i sea menor que la longitud de string, es decir, se va a detener cuando haya iterado sobre todos los caracteres de string. "i++" es la expresión de incremento, pues en cada iteración el valor de i aumentará uno para así poder avanzar. 
-//for (let i=0; i < string.length; i++) {
-    //aquí estamos asignando la variable letter para cada letra (caracter) del string, que se define dependiendo de su posición al momento de iterar, eso gracias al i entre corchetes. 
-  //  let letter = string[i];
-    //como usaremos ASCII para hacer el cifrado, creamos una variable que almacene el valor ASCII de cada letra, que se va a recuperar con el método charCodeAt
-    //let letterASCII = letter.charCodeAt();
-//}
+decodeBtn.addEventListener('click', caesarDecipher)
