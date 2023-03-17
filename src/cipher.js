9667afec5e7f5448d7f9eb160455ed75c0a80c96
Marissa Vargas <marissa.lorena@hotmail.com>
2023-03-17 14:19:42 -0600

    Agregué la excepción TypeError y corregí el código ascii para el objeto decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,12 @@
 const cipher = {
   
   encode: function(offset, string) {
+    if (typeof Number(offset) !== "number" || typeof string !== "string") {
+      throw new TypeError("The arguments must be a string and a number.");
+    }
     let result = "";
     for (let i=0; i<string.length; i++) {
-      let letterASCII = string[i].charCodeAt();
+      const letterASCII = string[i].charCodeAt();
       if (letterASCII >= 65 && letterASCII <= 90) {
         result += String.fromCharCode(((letterASCII - 65 + Number(offset))%26)+65);
       } else if (letterASCII >= 97 && letterASCII <= 122) {
@@ -14,15 +17,21 @@ const cipher = {
   },
 
   decode: function(offset, string) {
+    if (typeof Number(offset) !== "number" || typeof string !== "string") {
+      throw new TypeError("The arguments must be a string and a number.");
+    }
+    while (offset > 26) {
+      offset -= 26;
+    }
     let result = "";
-      for (let i=0; i<string.length; i++) {
-        let letterASCII = string[i].charCodeAt();
-        if (letterASCII >= 65 && letterASCII <= 90) {
-          result += String.fromCharCode(((letterASCII - 65 - Number(offset) + 26)%26)+65);
-        } else if (letterASCII >= 97 && letterASCII <= 122) {
-          result += String.fromCharCode(((letterASCII - 97 - Number(offset))%26)+97);
-        }
+    for (let i=0; i<string.length; i++) {
+      const letterASCII = string[i].charCodeAt();
+      if (letterASCII >= 65 && letterASCII <= 90) {
+        result += String.fromCharCode(((letterASCII - 65 - Number(offset) + 26)%26)+65);
+      } else if (letterASCII >= 97 && letterASCII <= 122) {
+        result += String.fromCharCode(((letterASCII - 97 - Number(offset) + 26)%26)+97);
       }
+    }
     return result;
   }
 };
--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import cipher from './cipher.js';
 console.log(cipher);
 
 const encodeBtn = document.getElementById('codeBtn');
-  encodeBtn.addEventListener('click', function(){
-    const string = document.getElementById('inputMessage').value;
-    const offset = document.getElementById('offset').value;
-    const resultSpan = document.getElementById('resultSpan');
+encodeBtn.addEventListener('click', function(){
+  const string = document.getElementById('inputMessage').value;
+  const offset = document.getElementById('offset').value;
+  const resultSpan = document.getElementById('resultSpan');
 
-    resultSpan.textContent = cipher.encode(offset, string);
-  })
+  resultSpan.textContent = cipher.encode (offset, string);
+})
 
 const decodeBtn = document.getElementById('decodeBtn');
-  decodeBtn.addEventListener('click', function(){
-    const string = document.getElementById('inputMessage').value;
-    const offset = document.getElementById('offset').value;
-    const resultSpan = document.getElementById('resultSpan');
+decodeBtn.addEventListener('click', function(){
+  const string = document.getElementById('inputMessage').value;
+  const offset = document.getElementById('offset').value;
+  const resultSpan = document.getElementById('resultSpan');
 
-    resultSpan.textContent = cipher.decode(offset, string);
-  })
+  resultSpan.textContent = cipher.decode(offset, string);
+})
